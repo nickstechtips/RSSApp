0006917174d9cab3b1dc98b41e7950df0bc3142d
Nick Seeber <nick@nickstechtips.com>
2016-04-02 21:02:02 -0600

    First Commit
    Got it pulling in RSS feed for sites, grabbing title, date, description,
    and download link. Still need to do String parsing to grab episode
    number and download functionality, and search functionality and compare
    to current list of files, and styling.

---
+++ FeedReader.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.ServiceModel.Syndication;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace RSSApp
+{
+    class FeedReader
+    {
+        public SyndicationFeed reader(string url)
+        {
+
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+
+            return feed;
+        }
+        
+        public List<string> tFileReader(SyndicationFeed feed)
+        {
+            List<string> feedItems = new List<string>();
+            foreach (SyndicationItem item in feed.Items)
+            {
+
+                String title = item.Title.Text;
+                if (title != null)
+                {
+                    string videoTitle = ("Title: " + TitleParser(title));
+                    string category =("Category: " + item.Categories[0].Name);
+                    string uriLink = ("Uri: " + item.Links[1].Uri);
+                    feedItems.Add(videoTitle);
+                    feedItems.Add(category);
+                    feedItems.Add(uriLink);
+                }
+
+
+            }
+
+            return feedItems;
+
+
+        }
+
+        public List<string> TorrentTitle(SyndicationFeed feed)
+        {
+
+            List<string> list = new List<string>();
+            foreach (var item in feed.Items)
+            {
+                var title = item.Title.Text;
+                if (title != null) list.Add(TitleParser(title));
+            }
+            return list; 
+        }
+
+        public List<string> TorrentDate(SyndicationFeed feed)
+        {
+            
+            List<string> dateoutput = new List<string>();
+            foreach (var item in feed.Items)
+            {
+                dateoutput.Add(item.PublishDate.DateTime.Date.ToLongDateString());
+
+            }
+            return dateoutput;
+        }
+
+        public List<string> TorrecntDescription(SyndicationFeed feed)
+        {
+            List<string> torrentDescriptionList = new List<string>();
+            foreach (var item in feed.Items)
+            {
+                torrentDescriptionList.Add(item.Summary.Text);
+            }
+
+            return torrentDescriptionList;
+        }
+
+        public List<string> tvDescription(SyndicationFeed feed)
+        {
+            List<string> tvdescriptionlist = new List<string>();
+            foreach (var item in feed.Items)
+            {
+                tvdescriptionlist.Add(item.Id);
+            }
+            return tvdescriptionlist;
+        }
+
+        public List<string> downloadLink(SyndicationFeed feed)
+        {
+            List<string> downloadLinkList = new List<string>();
+            foreach (var item in feed.Items)
+            {
+                downloadLinkList.Add(item.Links[1].Uri.ToString());
+            }
+            return downloadLinkList;
+        }
+
+
+
+
+
+
+
+#region string Parser
+
+        public string TitleParser(string title)
+        {
+            String newtitle = String.Empty;
+            for (int i = 0; i < title.Length; i++)
+            {
+                newtitle = newtitle + title[i];
+                if (i > 100)
+                {
+                    i = title.Length;
+                }
+            }
+            return newtitle;
+        }
+    }
+#endregion
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace RSSApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TitleListBox.Items.Count > 0)
+            {
+                TitleListBox.Items.Clear();
+                DescriptionListBox.Items.Clear();
+                EpisodeListBox.Items.Clear();
+                downloadLinkListBox.Items.Clear();
+                urlDisplay.Items.Clear();
+            }
+            if (EZTVRBtn.IsChecked.Value)
+            {
+                string url = "https://eztv.ag/ezrss.xml";
+                FeedReader fReader = new FeedReader();
+                var reader = fReader.reader(url);
+                foreach (string title in fReader.TorrentTitle(reader))
+                {
+                    TitleListBox.Items.Add(title);
+
+                }
+
+                foreach (string date in fReader.TorrentDate(reader))
+                {
+                    EpisodeListBox.Items.Add(date);
+                }
+                foreach (string description in fReader.tvDescription(reader))
+                {
+                    DescriptionListBox.Items.Add(description);
+                }
+                foreach (var downloadLink in fReader.downloadLink(reader))
+                {
+                    downloadLinkListBox.Items.Add(downloadLink);
+                    downloadLinkListBox.Items.Add(downloadLink);
+                }
+            }
+            else if (YIFYRBtn.IsChecked.Value)
+            {
+                if (TitleListBox.Items.Count > 0)
+                {
+                    TitleListBox.Items.Clear();
+                    DescriptionListBox.Items.Clear();
+                    EpisodeListBox.Items.Clear();
+                    downloadLinkListBox.Items.Clear();
+                    urlDisplay.Items.Clear();
+                }
+                string url = "https://yts.ag/rss";
+                FeedReader fReader = new FeedReader();
+                var reader = fReader.reader(url);
+                foreach (string title in fReader.TorrentTitle(reader))
+                {
+                    TitleListBox.Items.Add(title);
+                }
+
+                foreach (string date in fReader.TorrentDate(reader))
+                {
+                    EpisodeListBox.Items.Add(date);
+                }
+                foreach (string description in fReader.TorrecntDescription(reader))
+                {
+                    DescriptionListBox.Items.Add(description);
+                }
+                foreach (var downloadLink in fReader.downloadLink(reader))
+                {
+                    downloadLinkListBox.Items.Add(downloadLink);
+                    downloadLinkListBox.Items.Add(downloadLink);
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+            //foreach (string fileType in fReader.TorrentFileType(url))
+            //{
+            //    FileTypeListBox.Items.Add(fileType);
+            //}
+
+
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (urlDisplay.Items.Count > 0)
+            {
+                urlDisplay.Items.Clear();
+            }
+            else
+            {
+
+                int downloadLinkIndex = downloadLinkListBox.SelectedIndex;
+                FeedReader fReader = new FeedReader();
+                List<string> links = new List<string>();
+                string url = "";
+                if (EZTVRBtn.IsChecked.Value)
+                {
+                    url = "https://eztv.ag/ezrss.xml";
+                }
+                else if (YIFYRBtn.IsChecked.Value)
+                {
+                    url = "https://yts.ag/rss";
+                }
+                var reader = fReader.reader(url);
+                int count = 0;
+                foreach (var link in fReader.downloadLink(reader))
+                {
+                    
+                    if (count == downloadLinkIndex)
+                    {
+                        string downloadLinkUrl = link;
+                        urlDisplay.Items.Add(downloadLinkUrl);
+                    }
+                    count++;
+
+                }
+            }
+
+        }
+
+        private void ListBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TitleListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //if (TitleListBox.SelectedItems.Count > 0)
+            //{
+            //    int indexSelect = 0;
+            //    DescriptionListBox.SelectedIndex(indexSelect);
+            //}
+        }
+    }
+}
